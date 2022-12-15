13ef6bce5442a9875192e8df7b6f7080048fc67e
Peter Aandahl <peter.aandahl@axis.com>
2022-12-15 18:16:44 +0100

    Day 08, both parts complete. Not pretty... Day 07 still not complete.

--- day08.py
+++ day08.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('test08.csv', mode='r') as file:
+with open('input08.csv', mode='r') as file:
     inputFile = csv.reader(file)
 
     row = []
@@ -58,30 +58,31 @@ with open('test08.csv', mode='r') as file:
                 visible_trees += 1
 
     print('Part 1: {}'.format(visible_trees))
-    print()
+
 
     # Part 2
 
-    t_visible = 1
-    b_visible = 1
-    l_visible = 1
-    r_visible = 1
     # Only interior trees will need checking since trees at the edge will always
     # 0 trees in at least one direction resulting in a scenic score of 0.
     # x,y are coordinates of trees to be checked
+    max_scene = 1
+
     for x in range(1, len(grid[0])-1):
         for y in range(1, len(grid)-1):
             check_tree = grid[y][x]
-            print('Check tree at y={}, x={} : Height:{}'.format(y,x,check_tree))
+            #print('Check tree at y={}, x={} : Height:{}'.format(y,x,check_tree))
             view = 1
             scene = 1
+            scene_score = 1
             for b in range(y + 1, len(grid)-1):
                 #print('Trees below are {}'.format(grid[b][x]))
                 if grid[b][x] >= check_tree:
                     view = 0
                 if view:
                     scene += 1
-            print('Scene downwards is {}'.format(scene))
+            #print('Scene downwards is {}'.format(scene))
+            scene_score = scene_score * scene
+
             view = 1
             scene = 1
             for r in range(x+1, len(grid[0])-1):
@@ -90,4 +91,32 @@ with open('test08.csv', mode='r') as file:
                     view = 0
                 if view:
                     scene += 1
-            print('Scene right is {}'.format(scene))
+            #print('Scene right is {}'.format(scene))
+            scene_score = scene_score * scene
+
+            view = 1
+            scene = 1
+            for l in range(x-1, 0, -1):
+                #print('Trees to the right are {}'.format(grid[y][l]))
+                if grid[y][l] >= check_tree:
+                    view = 0
+                if view:
+                    scene += 1
+            #print('Scene left is {}'.format(scene))
+            scene_score = scene_score * scene
+
+            view = 1
+            scene = 1
+            for t in range(y - 1, 0, -1):
+                #print('Trees below are {}'.format(grid[b][x]))
+                if grid[t][x] >= check_tree:
+                    view = 0
+                if view:
+                    scene += 1
+            #print('Scene upwards is {}'.format(scene))
+            scene_score = scene_score * scene
+
+            if scene_score>=max_scene:
+                max_scene = scene_score
+
+    print('Part 2: {}'.format(max_scene))
